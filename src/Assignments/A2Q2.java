77b3062c80d5ecde156a17580bf3e085dd714fab
bosta6892 <bosta6892@HRH-IC0027333.wrdsb.ca>
2018-02-15 10:47:34 -0500

    Assignemnt 1 complete and assignment 2 in progress

---
+++ src/Assignments/A2Q2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignments;
+
+import becker.robots.City;          //COPY PASTA IMPORTS
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
+import java.awt.Color;
+/**
+ *
+ * @author bosta6892
+ */
+public class A2Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
