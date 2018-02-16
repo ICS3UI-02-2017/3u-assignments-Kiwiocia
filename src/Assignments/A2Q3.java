a5298503998d147ba8d627d5b5e703e2928d2041
bosta6892 <bosta6892@HRH-IC0027333.wrdsb.ca>
2018-02-16 11:21:27 -0500

    figure out #3

--- src/Assignments/A2Q3.java
+++ src/Assignments/A2Q3.java
@@ -4,6 +4,9 @@
  */
 package Assignments;
 
+import becker.robots.City;          //COPY PASTA IMPORTS
+import becker.robots.Direction;
+import becker.robots.RobotSE;
 /**
  *
  * @author bosta6892
@@ -14,6 +17,40 @@ public class A2Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        City meme = new City(); //I EXIST
+        RobotSE AB = new RobotSE(meme, -10, -2, Direction.SOUTH);
+       
+     if(AB.getStreet()!=0){
+           
+        while(AB.getStreet()>0){
+            while(AB.getDirection()!= Direction.NORTH){
+               AB.turnLeft();
+            }
+            AB.move();
+        }
+        while(AB.getStreet()<0){
+            while(AB.getDirection()!=Direction.SOUTH){
+                AB.turnLeft();
+            }
+            AB.move();
+      }
+     }   
+        
+     if(AB.getAvenue()!=0){
+       while(AB.getAvenue()>0){
+            while(AB.getDirection()!= Direction.WEST){
+                AB.turnLeft();
+            }
+            AB.move();
+        }
+    
+        while(AB.getAvenue()<0){
+            while(AB.getDirection()!=Direction.EAST){
+                
+                      }
+       AB.move();
+      }
+     }           
     }
-}
+   }
