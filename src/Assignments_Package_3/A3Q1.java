ce2fc7cde29ed4e30bd4ba5b63d733344aebb513
bosta6892 <bosta6892@HRH-IC0027333.wrdsb.ca>
2018-02-26 10:38:35 -0500

    Assigment 3 done

--- src/Assignments_Package_3/A3Q1.java
+++ src/Assignments_Package_3/A3Q1.java
@@ -20,7 +20,7 @@ public class A3Q1 {
      */
     public static void main(String[] args) {
         City Doggo = new City();           //city         
-        RobotSE Doc = new RobotSE(Doggo, 0, 2, Direction.WEST); //Hello Doc
+        RobotSE Doc = new RobotSE(Doggo, 0, 2, Direction.WEST);
         Doc.setLabel("+");
         new Wall(Doggo, 1, 1, Direction.NORTH); //build a square
         new Wall(Doggo, 1, 2, Direction.NORTH);
@@ -30,5 +30,19 @@ public class A3Q1 {
         new Wall(Doggo, 2, 1, Direction.WEST);
         new Wall(Doggo, 2, 1, Direction.SOUTH);
         new Wall(Doggo, 2, 2, Direction.SOUTH);
+
+        int ouo = 0;
+        while (ouo < 2) {
+            Doc.move(2);     //i just copy pasted the code from assignemtn 1
+            Doc.turnLeft();
+            Doc.move(3);
+            Doc.turnLeft();
+            Doc.move(3);
+            Doc.turnLeft();
+            Doc.move(3);
+            Doc.turnLeft();
+            Doc.move();
+            ouo = ouo + 1;
+        }
     }
 }
