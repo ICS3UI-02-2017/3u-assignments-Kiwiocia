06e668cae9fec717a6711d19d1bc8db46907b760
bosta6892 <bosta6892@HRH-IC0027333.wrdsb.ca>
2018-02-26 11:13:02 -0500

    Assigment 3 started

--- src/Assignments_Package_3/A3Q3.java
+++ src/Assignments_Package_3/A3Q3.java
@@ -21,6 +21,25 @@ public class A3Q3 {
      */
     public static void main(String[] args) {
         City England = new City();
-        RobotSE Pupper = new RobotSE(England, 1, 1, Direction.EAST);
+        RobotSE Pupper = new RobotSE(England, 1, 1, Direction.EAST, 20);
+
+        int pepe = 0;               //new integer called pepe
+        while (pepe < 20) {         //while pepe is less then 20, loop
+            Pupper.putThing();      //put a thing and move
+            Pupper.move();
+            if (Pupper.getAvenue() >= 6) { //if x location is greater than 6
+                Pupper.turnRight();        //move down once and turn right
+                Pupper.move();             //which creates a zig zag motion
+                Pupper.turnRight();
+                Pupper.move();
+            }
+            if (Pupper.getAvenue() <= 0) { //same thing as previous 
+                Pupper.turnLeft();         //but for a x location that is less
+                Pupper.move();             //than or equal to 0
+                Pupper.turnLeft();
+                Pupper.move();
+            }
+            pepe = pepe + 1;               //add 1 to pepe
+        }
     }
 }
