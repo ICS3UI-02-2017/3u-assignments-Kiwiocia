40565890ac8779913dcc0084f6bd6c302e1c707d
bosta6892 <bosta6892@HRH-IC0027333.wrdsb.ca>
2018-04-20 11:51:13 -0400

    Assignment 6

--- src/Assignment_Package_6/A6Q7.java
+++ src/Assignment_Package_6/A6Q7.java
@@ -12,15 +12,25 @@ public class A6Q7 {
 
     public static void main(String[] args) {
 
-
-
-
         double[] array = new double[999];
         for (int x = 0; x < 999; x++) {
             array[x] = (x + 2);
         }
-        for (int a = 0; a < 999; a++) {
 
+        for (int a = 0; a < 999; a++) {
+            if (array[a] == 0) {
+            } else {
+                for (int z = 1; z < 999; z++) {
+                    if (array[z] % array[a] == 0) {
+                        array[z] = array[z] * 0;
+                    }
+                }
+            }
+        }
+        for (int y = 0; y < 999; y++) {
+            if (array[y] != 0) {
+                System.out.println(array[y]);
+            }
         }
 
 
